c61b9dd57342e1e44799a29198b376cb4b778a35
Daniel <Daniel@Daniel-PC>
2018-05-14 19:16:49 -0500

    Se crea mascota.idl, la ClasePrincipal y ConectarMysql.

---
+++ src/ClasePrincipal.java
@@ -0,0 +1,10 @@
+import conexion.*;
+public class ClasePrincipal {
+
+    public static void main(String[] args) {
+        conexion conectar = new conexion();
+        
+        conectar.conexion();
+    }
+    
+}
---
+++ src/ConectarMysqlJava.java
@@ -0,0 +1,6 @@
+import conexion.*;
+public class ConectarMysqlJava {
+    conexion conectar = new conexion();
+    
+    
+}
